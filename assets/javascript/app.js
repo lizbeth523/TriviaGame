159d66ff5a0ebdce4cbc8ea929d1f0b8afdb8af9
Elizabeth Tom <elizabeth_tom@excite.com>
2017-08-27 23:53:12 -0500

    update 8/27/17 23:53

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,17 +17,20 @@ $(document).ready ( function() {
 			question: "Which of the following is not one of the Hawaiian islands?",
 			answers: ["Oahu",
 					  "Maui",
-					   "Ahupuaa",
+					   "Ohana",
 					   "Kauai"],
-			correctIndex: 2
+			correctIndex: 2,
+			comment: "Part of Hawaiian culture, ʻohana means family. The concept emphasizes that families are bound together and members must cooperate and remember one another.",
+			mediaSrc: "assets/images/ohana.mp4"
 		},
 		{
-			question: "How are hurricanes named",
+			question: "How are hurricanes named?",
 			answers: ["By the World Meteorological Organization",
 					  "By the president, once every four years",
 					  "Hurricane forecasters draw names from a hat",
 					  "Friends or realitives of hurricane forecasters"],
-			correctIndex: 0
+			correctIndex: 0,
+			comment: "NOAA's National Hurricane Center does not control the naming of tropical storms. Instead, there is a strict procedure established by the World Meteorological Organization. For Atlantic hurricanes, there is a list of male and female names which are used on a six-year rotation."
 		},
 		{
 			question: "Which island has a sightseeing train that once transported sugar?",
@@ -35,7 +38,9 @@ $(document).ready ( function() {
 					  "Jamaica",
 					  "St. Kitts",
 					  "Martinique"],
-			correctIndex: 2
+			correctIndex: 2,
+			comment: "The St. Kitts Scenic Railway transported sugar cane from plantations to the capital city of Basseterre. Today, vintage narrow-gauge trains offer a delightful tour along coastlines, lush fields, and the ruins of the old plantations (some of which are now lovely inns, like Ottley’s Plantation).",
+			mediaSrc: "assets/images/StKittsRailway.mp4"
 		},
 		{
 			question: "Where was the famous “Painkiller” cocktail created?",
@@ -43,7 +48,8 @@ $(document).ready ( function() {
 					  " Antigua",
 					  "Tortola",
 					  "Jost Van Dyke"],
-			correctIndex: 3
+			correctIndex: 3,
+			comment: "This potent mix of Pusser’s rum, pineapple juice, cream of coconut, and orange juice, topped with grated fresh nutmeg was invented at the Soggy Dollar Bar on the crescent-shaped White Bay Beach of Jost Van Dyke in the 1970’s. Wade in or swim up from a boat, hand over some soggy bills, then sip away in a hammock or in the open-air beachfront bar."
 		},
 		{
 			question: "The French never ruled which of the following islands?",
@@ -51,7 +57,8 @@ $(document).ready ( function() {
 					  "Grenada",
 					   "St. Lucia",
 					   "St. Croix"],
-			correctIndex: 3
+			correctIndex: 3,
+			comment: "St. Croix is one of three U.S. Virgin Islands (the others are St. Thomas and St. John) colonized by the Danish in the 17th century, but purchased by the U.S. in 1917 under President Theodore Roosevelt. French place names still linger on St. Lucia, Dominica and Grenada, even though all later became British colonies."
 		},
 		{
 			question: "What country’s coastal waters are protected as a national park?",
@@ -59,13 +66,15 @@ $(document).ready ( function() {
 					  "Bonaire",
 					  "Cayman Islands",
 					  "Tortola"],
-			correctIndex: 1
+			correctIndex: 1,
+			comment: "The coastal waters of Bonaire were declared the Bonaire Marine Park in 1979. It’s a superb diving destination for spotting colorful fish, coral, and rays."
 		},
 		{
 			question: "Tropical rainforests are home to half the plant and animal species on Earth. True or False?",
 			answers: ["True",
 					  "False"],
-			correctIndex: 0
+			correctIndex: 0,
+			comment: "Although they cover less than 2 percent of Earth's surface, rainforests house an estimated 50 percent of all life on the planet's land masses."
 		},
 		{
 			question: "Trees in the tropical rainforest are so dense that it takes approximately ___ minutes for rainfall to reach the ground.",
@@ -73,11 +82,12 @@ $(document).ready ( function() {
 					  "10",
 					  "30",
 					  "60"],
-			correctIndex: 1
+			correctIndex: 1,
+			comment: "Much of the rain is stopped by the thick foliage in the canopy layer. Most trees in the forest grow to this height. There are plants that grow in the canopy layer. Their roots don't reach the ground. These are called air plants."
 		}
 	];
 	var secondsPerQuestion = 5;
-	var secondsPerResultDisp = 3;
+	var secondsPerResultDisp = 5;
 	var timeRemaining;
 
 	// Start button click handler. Starts the game.
@@ -101,7 +111,7 @@ $(document).ready ( function() {
 
 	// Update display and decrement timeRemaining as the countdown progresses
 	var countdown = function() {
-		$("#time-remaining").text(timeRemaining);
+		$("#time-remaining").html(timeRemaining);
 		console.log(timeRemaining);
 		timeRemaining--;
 		if (timeRemaining <= 0)
@@ -138,13 +148,17 @@ $(document).ready ( function() {
 		timeRemaining = secondsPerResultDisp;
 		intervalId = setInterval(countdown, 1000);
 		correctIndex = questions[questionIndex].correctIndex;
+		$(".game-div").html("<h2><span id='result'></span> <span id='msg'></span></h2>");
+		$(".game-div").append("<video width='640' height='480' autoplay><source src=" + questions[questionIndex].mediaSrc + " type='video/mp4'></source></video>");
 		if (answerIndex === correctIndex)
 		{
-			$(".game-div").html("<h2>Correct!</h2>");
+			$("#result").html("Correct!");
+			$("#msg").html("<h2>" + questions[questionIndex].comment + "</h2>");
 		}
-		else
+		else 
 		{
-			$(".game-div").html("<h2>Nope! The correct answer is " + questions[questionIndex].answers[correctIndex]  + "</h2>");
+			$("#result").html("Nope!");
+			$("#msg").html("<h2>The correct answer is " + questions[questionIndex].answers[correctIndex]  + "</h2>");
 		}
 	};
 
